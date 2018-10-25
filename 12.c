3823d591f9042289f218d63a181af26e65c2f7be
magistrkoljan <olawed@gmail.com>
2018-10-25 18:25:04 +0300

    id + EUID, EGID, text, groups

--- 12.c
+++ 12.c
@@ -1,11 +1,26 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <pwd.h>
+#include <grp.h>
+#include <limits.h>
 
 int main()
-{
-  printf("PID:%d,\nPPID:%d,\nPGID:%d,\nUID:%d,\nGID:%d,\nSID:%d\n",getpid(), getppid(), getpgrp(), getuid(), getgid(), getsid(0));
- return 0;
+{ 
+  gid_t a[NGROUPS_MAX];
+  int i;
+  printf("PID:%d,\n",getpid());
+  printf("PPID:%d,\n", getppid());
+  printf("PGID:%d,\n", getpgrp());
+  printf("UID:%d(%s),\n",  getuid(), getpwuid(getuid())->pw_name);
+  printf("GID:%d(%s),\n",getgid(), getgrgid(getgid())->gr_name);
+  printf("SID:%d,\n",getsid(0));
+  printf("EUID:%d(%s),\n",geteuid, getpwuid(geteuid())->pw_name);
+  printf("EGID:%d(%s),\n",getegid, getgrgid(getegid())->gr_name);
+  printf("GROUPS:\n");
+  for  (i=0;i<getgroups(NGROUPS_MAX, a);i++)
+      printf("%d(%s)\n",a[i],getgrgid(a[i])->gr_name);
+ return 0; 
 
 }
  
