0a15fc0acd3a919f2f1a8ca31a7d26ab9c5b097a
magistrkoljan <olawed@gmail.com>
2018-10-25 19:05:16 +0300

    13 task. +signals of result

--- 13.c
+++ 13.c
@@ -2,8 +2,27 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <pwd.h>
+#include <grp.h>
+#include <limits.h>
 
-
+int id(void)
+{
+  gid_t a[NGROUPS_MAX];
+  int i;
+  printf("PID:%d,\n",getpid());
+  printf("PPID:%d,\n", getppid());
+  printf("PGID:%d,\n", getpgrp());
+  printf("UID:%d(%s),\n",  getuid(), getpwuid(getuid())->pw_name);
+  printf("GID:%d(%s),\n",getgid(), getgrgid(getgid())->gr_name);
+  printf("SID:%d,\n",getsid(0));
+  printf("EUID:%d(%s),\n",geteuid, getpwuid(geteuid())->pw_name);
+  printf("EGID:%d(%s),\n",getegid, getgrgid(getegid())->gr_name);
+  printf("GROUPS:\n");
+  for  (i=0;i<getgroups(NGROUPS_MAX, a);i++)
+      printf("%d(%s)\n",a[i],getgrgid(a[i])->gr_name);
+ return 0; 
+}
 
 int main()
 {
@@ -17,9 +36,38 @@ int main()
     {
       int result;
       wait(&result);
-      printf("Parent process:\nPID:%d,\nPPID:%d,\nPGID:%d,\nUID:%d,\nGID:%d,\nSID:%d,\nRESULT:%d \n",getpid(), getppid(), getpgrp(), getuid(), getgid(), getsid(0), result);
+      printf("Parent process:\n");
+      if (id()!=0)
+       {
+       perror("Failed to id");
+       return 1;
+          }
+     printf("FINISH STATUS:");
+     if (result==0)
+        printf("OK");
+     else if (WIFEXITED)
+        printf("the child terminated normally, that is, by calling exit(3) or _exit(2), or by returning from main()");
+          if (WIFSIGNALED)
+          {
+            printf("the child process was terminated by a signal");
+            if (WCOREDUMP)
+               printf("the child produced  a  core  dump");
+          }
+          if (WIFSTOPPED)
+              printf("the child process was stopped by delivery of a signal");
+          if (WIFCONTINUED)
+               printf("the  child  process  was resumed by delivery of SIGCONT");
+
     }
-  else printf("Child process:\nPID:%d,\nPPID:%d,\nPGID:%d,\nUID:%d,\nGID:%d,\nSID:%d\n",getpid(), getppid(), getpgrp(), getuid(), getgid(), getsid(0));
+  else {
+         printf("Child process:\n");
+         if (id()!=0)
+          {
+       perror("Failed to id");
+       return 1;
+          }
+       }
+}
 
 return 0;
 
