336a5ebba2c9708331a512d32817af640f71d69c
magistrkoljan <olawed@gmail.com>
2018-11-30 15:54:53 +0300

    9 Change lu->float

--- 9.c
+++ 9.c
@@ -8,13 +8,13 @@
 
 int main() {
 	struct statfs stat;
-	if (statfs(".", &stat))
+	if (statfs(".", &stat))//argv add
  	{
 		perror("Failed to statfs\n");
 	        return(1);
 	}
-	printf("Total space: %luGB\n",(stat.f_blocks * stat.f_bsize) >> 30);
-	printf("Free space: %luGb\n", (stat.f_bfree * stat.f_bsize) >> 30);
+	printf("Total space: %.1fGB\n",(stat.f_blocks * stat.f_bsize)/1.0e9f);//вывести UID
+	printf("Free space: %.1fGb\n", (stat.f_bfree * stat.f_bsize)/1.0e9f);
 	return(0);
 }
 
